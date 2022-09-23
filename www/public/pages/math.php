0c9474964c888d176e8f8ff08b990c340d911689
KarmaSora <karammtr@gmail.com>
2022-09-23 11:31:01 +0200

    added a simple calculator ish, for uppgift 4

---
+++ www/public/pages/math.php
@@ -0,0 +1,22 @@
+<?php
+
+
+function sum($num1, $num2){
+   $sum = $num1 + $num2;
+   return $sum;
+}
+
+function multiply($num1, $num2){
+    $prod = $num1 * $num2;
+    return $prod;
+}
+function divide($num1, $num2){
+    $div = $num1 / $num2;
+    return $div;
+}
+function subtr($num1, $num2){
+    $sub = $num1 - $num2;
+    return $sub;
+}
+
+?>
---
+++ www/public/simpleCalc.php
@@ -0,0 +1,12 @@
+<?php
+    include ('pages/math.php');
+
+    $sumAns =sum( 4, 4);
+    $divAns = divide( 4, 4);
+    $subAns =subtr(4, 4);
+    $mulAns = multiply(4, 4);
+
+    echo("this here is a simple calc using the number 4. <br> the sum of 4 +4 = $sumAns <br> the divitsion of 4/4 = $divAns  <br> the subtraction of 4-4 = $subAns  <br> the prod of 4*4 = $mulAns");
+
+?>
+
