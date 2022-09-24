486413c47e8daa0e1c724ab34d9766ef458b9ed2
KarmaSora <karammtr@gmail.com>
2022-09-24 13:29:28 +0200

    creating a loggin/ID type of page, but docker is not woriking

---
+++ www/public/pages/personID.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Person account</title>
+</head>
+<body>
+    <header><h1>person account</h1></header>
+    <main><article>
+        <p>insert your forname</p>
+        <input type="text" name="forName" >
+        <br>
+        <p>insert your aftername</p>
+        <input type="text" name="afterName" >
+        <br>
+        <p>insert age</p>
+        <input type="number" name="age">
+        <br>
+        <p>password</p>
+        <input type="text" name="pass">
+
+    </article></main>
+    <footer><h3>contact: abc@mail.com <br>like and support </h3></footer>
+    
+    <?php
+
+    ?>
+</body>
+</html>
---
+++ www/public/pages/personLog.php
@@ -0,0 +1,8 @@
+<?php
+
+$forname = $_POST("forname");
+$aftername = $_POST('aftername');
+$pass = $_POST("pass");
+$age = $_POST('age');
+
+?>
