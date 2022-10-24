a0f786462bd20f22dee40ec3d8772d053a9f5b7f
KarmaSora <karammtr@gmail.com>
2022-10-24 12:00:38 +0200

    figure why code isnt working as it should, contorl naming of variables

--- www/public/upp9/userControl.php
+++ www/public/upp9/userControl.php
@@ -2,15 +2,18 @@
     // För att förenkla användandet av mb_ funktioner
     mb_internal_encoding("UTF-8");     
     $file = "person.dat";
-
-    if(file_exists($file))
-    $personArray = unserialize(file_get_contents($file));
-
+    include("Person.php");
 
 if(!isset($_POST['user'])){
     header("location: login.html");
     exit;
 }
+
+    if(file_exists($file))
+    $personArray = unserialize(file_get_contents($file));
+
+
+
 
 for($i=0; $i<count($personArray); $i++){
     if($user == $personArray[$i]->getUserName() && $pwd == $personArray[$i]->getPassWord()){
